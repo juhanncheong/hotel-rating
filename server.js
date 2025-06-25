8a578e958f42f234cda25d5a04d5efd29e81eeab
Robert Cheong <hjim1188@gmail.com>
2025-06-25 15:25:29 +0800

    fix serverjs

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', userRoutes);
-app.use('/api', adminRoutes);
+app.use('/api/admin', adminRoutes);
 app.use('/api/user', require('./routes/userRoutes'));
 
 // Test route
