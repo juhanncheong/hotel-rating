cc415e2b5b5328aac728b4d3b22ce225326c1edc
Robert Cheong <hjim1188@gmail.com>
2025-06-30 15:46:11 +0800

    k

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const adminRoutes = require('./routes/admin');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use("/api/admin", adminRoutes);
