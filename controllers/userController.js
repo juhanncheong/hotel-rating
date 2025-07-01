3cd323e51fabf00b80b9702aa4438241a4bd9022
Robert Cheong <hjim1188@gmail.com>
2025-07-01 13:11:51 +0800

    k

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,8 @@
 const User = require('../models/User');
 const InvitationCode = require('../models/InvitationCode');
 const axios = require('axios');
-const { registerUser, updateUserBalance, getAllUsers } = require("../controllers/userController");
-const { getUserById } = require("../controllers/userController");
+const userController = require("../controllers/userController");
+
 
 function getVipRank(amount) {
   if (amount >= 5000) return "Gold";
