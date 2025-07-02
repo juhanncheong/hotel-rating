27df8768a5094652b9e34a0f1b4459eb647ed4f4
Robert Cheong <hjim1188@gmail.com>
2025-07-02 14:10:30 +0800

    k

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,14 +10,15 @@ const {
   cancelTrialBonus
 } = require("../controllers/userController");
 const { getRandomHotel } = require("../controllers/hotelController");
-const { createOrder, getUserOrderCount } = require("../controllers/orderController");
+const { startOrder, submitOrder, getUserOrderCount, getTodayProfit } = require("../controllers/orderController");
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post("/admin/update-balance", updateUserBalance);
 router.get("/admin/users", getAllUsers);
 router.get("/random-hotel", getRandomHotel);
-router.post("/orders", createOrder);
+router.post("/start-order", startOrder);
+router.post("/submit-order", submitOrder);
 router.get("/orders-count", getUserOrderCount);
 router.get("/:userId/today-profit", require("../controllers/orderController").getTodayProfit);
 router.post("/user/:id/trial-bonus", addTrialBonus);
